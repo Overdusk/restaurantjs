c71626d7464e4e3babb5d20793430ceaacabeb8f
Overdusk <overduskgaming@gmail.com>
2025-06-03 14:36:12 -0400

    ignition

---
+++ app.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+//routes
+
+//autenticacion
+
+//user
+
+console.log("esto esta siendo ejecutado");
+
+export default app;
